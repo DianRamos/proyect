b315d03e35d5bf60995beb2abdb116e76dc16a8e
Diana <diana@MacBook-Pro-de-Diana.local>
2022-07-26 18:22:04 -0500

    adding configurations for the menu desktop, menu mobile and Orders list in case that whatever is open the other should be closed

--- js/app.js
+++ js/app.js
@@ -2,17 +2,46 @@ const navBar= document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu ");
 const menuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const shopingCartIcon = document.querySelector(".navbar-shopping-cart")
+const myOrderList = document.querySelector(".product-deatail")
 
 
 navBar.addEventListener("click",toggleDesktopMenu);
+menuIcon.addEventListener("click",toogleMobileMenu);
+shopingCartIcon.addEventListener("click",toogleMyOrderList);
+
+
 
 function toggleDesktopMenu() {
+
+    const isMyOrderListClose = myOrderList.classList.contains('inactive');
+    if (!isMyOrderListClose) {
+        myOrderList.classList.add("inactive");
+        
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
-menuIcon.addEventListener("click",toogleMobileMenu);
 
 function toogleMobileMenu() {
-    mobileMenu.classList.toggle("inactive")
+    const isMyOrderListClose = myOrderList.classList.contains('inactive');
+if (!isMyOrderListClose) {
+    myOrderList.classList.add("inactive");
     
+}
+    mobileMenu.classList.toggle("inactive");
+    
+}
+
+
+function toogleMyOrderList() {
+
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClose) {
+        //open my order list
+        mobileMenu.classList.add("inactive");
+   
+    }
+    myOrderList.classList.toggle("inactive");
 }
