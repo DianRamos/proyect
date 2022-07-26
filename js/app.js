222016d6bb4c19db61a0321e35ccbc2254a3422c
Diana <diana@MacBook-Pro-de-Diana.local>
2022-07-26 16:29:10 -0500

    adding menu mobile to the nav-bar and the mediaQ. for desktop and mobile

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const navBar= document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu ");
+const menuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
 navBar.addEventListener("click",toggleDesktopMenu);
-const desktopMenu = document.querySelector(".desktop-menu ")
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+menuIcon.addEventListener("click",toogleMobileMenu);
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
+    
 }
