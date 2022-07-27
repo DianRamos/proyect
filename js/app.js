2537fa835b23be8b6612c499f7af8f6526213d93
Diana <diana@MacBook-Pro-de-Diana.local>
2022-07-27 15:56:31 -0500

    add product list to the home, and create an object for the product list in the file app.js, to add styles for product list in the file index,css

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ const navBar= document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu ");
 const menuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const shopingCartIcon = document.querySelector(".navbar-shopping-cart")
-const myOrderList = document.querySelector(".product-deatail")
+const shopingCartIcon = document.querySelector(".navbar-shopping-cart");
+const myOrderList = document.querySelector(".product-deatail");
+const cardsContainer = document.querySelector(".cards-container");
 
 
 navBar.addEventListener("click",toggleDesktopMenu);
@@ -45,3 +46,85 @@ function toogleMyOrderList() {
     }
     myOrderList.classList.toggle("inactive");
 }
+
+const productList = [];
+productList.push({
+    name: "Car",
+    price:120,
+    image: "https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=800",
+},
+{
+    name: "Bike",
+    price:120,
+    image:"https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+{
+    name: "Bowl",
+    price:120,
+    image:"https://images.pexels.com/photos/951334/pexels-photo-951334.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+{
+    name: "Guitar",
+    price:120,
+    image:"https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+{
+    name: "Chair",
+    price:120,
+    image:"https://images.pexels.com/photos/8989497/pexels-photo-8989497.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+{
+    name: "Table",
+    price:120,
+    image:"https://images.pexels.com/photos/1209776/pexels-photo-1209776.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+{
+    name: "Soccer ball",
+    price:120,
+    image:"https://images.pexels.com/photos/209861/pexels-photo-209861.jpeg?auto=compress&cs=tinysrgb&w=800",
+
+},
+);
+function renderProducts(arr){
+    for(product of arr){const productCard = document.createElement('div');
+    
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    
+    const separatorDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText='$'+product.price;
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    separatorDiv.appendChild(productPrice);
+    separatorDiv.appendChild(productName);
+
+    const separatorFigure = document.createElement('figure');
+
+    const imageIconCart = document.createElement('img');
+    imageIconCart.setAttribute('src','./assets/Platzi_YardSale_Icons/bt_add_to_cart.svg');
+
+    separatorFigure.appendChild(imageIconCart);
+
+    productInfo.appendChild(separatorDiv);
+    productInfo.appendChild(separatorFigure);
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+}
+renderProducts(productList);
